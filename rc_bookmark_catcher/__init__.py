752ddd9bc14d5a5193205125872d8c7c5fa5b383
Firas Wehbe <firaswehbe@users.noreply.github.com>
2021-09-22 19:10:04 -0500

    add Bootstrap basics

--- rc_bookmark_catcher/__init__.py
+++ rc_bookmark_catcher/__init__.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 def create_app(test_config=None):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
 
     @app.route("/")
     def index():
-        return f"Testing {__name__} \n\
-        Instant  = {app.instance_path}"
+        return render_template('base.html')
+    
+    Bootstrap(app)
 
     return app
 
