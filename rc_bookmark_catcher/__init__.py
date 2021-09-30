fd0addfd0e2005789a9e34bfdd5836ba36198c9c
Firas Wehbe <firaswehbe@users.noreply.github.com>
2021-09-30 17:27:29 -0500

    flash messages

--- rc_bookmark_catcher/__init__.py
+++ rc_bookmark_catcher/__init__.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
+    app.secret_key = b'deleteme123'
+
     @app.route("/")
     def index():
+        if request.args.get('pid') is None:
+            flash("A valid project id is required for this page")
         return render_template('base.html')
     
     Bootstrap(app)
