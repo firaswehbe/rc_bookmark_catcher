773283a1935fe285f6a01cc416af0a2d8f06f768
Firas Wehbe <firaswehbe@users.noreply.github.com>
2021-10-31 08:21:52 -0500

    updated relationships to handle key overlap

--- rc_bookmark_catcher/models.py
+++ rc_bookmark_catcher/models.py
@@ -1,50 +1,62 @@
 from . import db
 
+
+
 class Project(db.Model):
     
     # Primary Key
-    project_id = db.Column(db.Integer, primary_key=True,nullable=False)
+    project_id = db.Column(db.Integer, primary_key=True,nullable=False, autoincrement=False)
 
     # Other attributes
     project_title = db.Column(db.String(128))
     api_token = db.Column(db.String(128))
 
+    # Relationships -- you have to use back_populates and not backref so you can have asymmetric cascades
+    instruments = db.relationship('Instrument', back_populates='project', cascade = ['all', 'delete', 'delete-orphan'], order_by = 'Instrument.order_num')
+    variables = db.relationship('Field', back_populates='project', cascade = ['all', 'delete', 'delete-orphan'])
+
 
 class Instrument(db.Model):
 
     # Primary Keys (Composite)
-    project_id = db.Column(db.Integer, primary_key=True,nullable=False)
-    instrument_name = db.Column(db.String(128), primary_key=True,nullable=False)
+    project_id = db.Column(db.Integer, nullable=False )
+    instrument_name = db.Column(db.String(128), nullable=False)
 
     # Other attributes
     order_num = db.Column(db.Integer)
     instrument_label = db.Column(db.String(256))
 
-    # Relationships
-    project = db.relationship(Project, backref='instruments')
-
     # Constraints
     __table_args__ = (
-        db.ForeignKeyConstraint(['project_id'], [Project.project_id]),
+        db.PrimaryKeyConstraint('project_id','instrument_name'),
+        db.ForeignKeyConstraint(['project_id'], ['project.project_id']),
     )
 
-
+    # Relationships -- you have to use back_populates and not backref so you can have asymmetric cascades
+    # not sure how I got the overlaps arguments to work, did trial and error
+    project = db.relationship('Project', back_populates = 'instruments', overlaps = 'instruments')
+    variables = db.relationship('Field', back_populates = 'instrument', overlaps = 'variables')
 
 class Field(db.Model):
 
     # Primary Keys (Composite)
-    project_id = db.Column(db.Integer, primary_key=True,nullable=False)
-    form_name = db.Column(db.String(128), primary_key=True,nullable=False)
-    field_name = db.Column(db.String(128), primary_key=True,nullable=False)
+    project_id = db.Column(db.Integer, nullable=False )
+    field_name = db.Column(db.String(128), nullable=False)
 
     # Other attributes
     order_num = db.Column(db.Integer)
-    field_label = db.Column(db.String(256))
-
-    # Relationship
-    instrument = db.relationship( Instrument )
+    form_name = db.Column(db.String(128), nullable=False)
+    field_label = db.Column(db.Text)
 
     # Constraints
-    __table_args__ = ( 
-        db.ForeignKeyConstraint(['project_id', 'form_name'], [Instrument.project_id,Instrument.instrument_name]),
+    __table_args__ = (
+        db.PrimaryKeyConstraint('project_id', 'field_name'),
+        db.ForeignKeyConstraint(['project_id'], ['project.project_id']),
+        db.ForeignKeyConstraint(['project_id', 'form_name'], ['instrument.project_id','instrument.instrument_name']),
     )
+
+    # Relations -- you have to use back_populates and not backref so you can have asymmetric cascades
+    # not sure how I got the overlaps arguments to work, did trial and error
+    instrument = db.relationship('Instrument', back_populates = 'variables', overlaps = 'variables' )
+    project = db.relationship('Project', back_populates = 'variables', overlaps = 'instrument,variables')
+
